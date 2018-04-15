4eae7c196ed11cb94de5fc05bb313e6f5920a3da
JinCheng <JinChengJoker@foxmail.com>
2018-04-15 23:28:47 +0800

    add dropdownMenu

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ window.onscroll = function() {
     }
 }
 
+listenNavBar()
+
 portfolio1.onclick = function() {
     filterBar.className = "filterBar filter1"
 }
@@ -14,4 +16,16 @@ portfolio2.onclick = function() {
 }
 portfolio3.onclick = function() {
     filterBar.className = "filterBar filter3"
+}
+
+function listenNavBar() {
+    let liTags = document.querySelectorAll('nav.navBar > ul > li')
+    for(let i = 0; i < liTags.length; i++) {
+        liTags[i].onmouseenter = function() {
+            this.classList.add('active')
+        }
+        liTags[i].onmouseleave = function() {
+            this.classList.remove('active')
+        }
+    }
 }
