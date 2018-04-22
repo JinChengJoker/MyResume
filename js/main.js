75aa7520af38e409d5da9ea48446ae4cfb782f5f
JinCheng <JinChengJoker@foxmail.com>
2018-04-22 14:59:01 +0800

    add fade-in

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ window.onload = function() {
     detectionLoad()
     highlightNavBar()
     listenNavBar()
+    listenFadeIn()
 }
 
 window.onscroll = function() {
     highlightNavBar()
+    listenFadeIn()
 }
 
 portfolio1.onclick = function() {
@@ -21,6 +23,17 @@ portfolio3.onclick = function() {
 function detectionLoad() {
     var loading = document.getElementById('loading')
     loading.classList.remove('active')
+}
+
+function listenFadeIn() {
+    let height = document.documentElement.clientHeight
+    let tags = document.getElementsByClassName('fadeIn')
+    for(let i = 0; i < tags.length; i++) {
+        let top = tags[i].getBoundingClientRect().top
+        if(height - top > 100) {
+            tags[i].classList.remove('offset')
+        }
+    }
 }
 
 function highlightNavBar() {
