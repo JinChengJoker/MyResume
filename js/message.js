9f5d5ebe27fd0ebaadef88bef253b49587291a42
JinCheng <JinChengJoker@foxmail.com>
2018-08-05 16:53:15 +0800

    基本留言功能

--- js/message.js
+++ js/message.js
@@ -6,13 +6,19 @@ AV.init({
 var Message = AV.Object.extend('Message')
 var message = new Message()
 
-message.set('name', 'Jin Cheng')
-message.set('content', 'Hello World!')
-message.save().then(
-    function(res) {
-        alert('LeanCloud Rocks!')
-    },
-    function(err) {
-        alert('提交失败！')
-    }
-)
+var messageForm = document.getElementById('messageForm')
+messageForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    var name = messageForm.querySelector('input[name=name]').value
+    var content = messageForm.querySelector('input[name=content]').value
+    message.set('name', name)
+    message.set('content', content)
+    message.save().then(
+        function(res) {
+            alert('提交成功！')
+        },
+        function(err) {
+            alert('提交失败！')
+        }
+    )
+})
